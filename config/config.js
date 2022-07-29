0243108dd91d6719e4779f0bf77d3bcbc8d8d959
Cole Gerdemann <corvettecole@gmail.com>
2022-07-28 22:27:53 -0400

    Added sudo administrator message and incident will be reported easter egg

--- config/config.js
+++ config/config.js
@@ -14,12 +14,12 @@
 
 var CONFIG = CONFIG || {};
 
-CONFIG.prompt = function(cwd, user) {
-   if (user)
-      return '<span class="user">' + user +
-          '</span>@<span class="host">clarkduvall.com</span>:<span class="cwd">' +
-          cwd + '</span>$ ';
-   return 'jsterm1.0 $ ';
+CONFIG.prompt = function (cwd, user) {
+    if (user)
+        return '<span class="user">' + user +
+            '</span>@<span class="host">corvettecole.com</span>:<span class="cwd">' +
+            cwd + '</span>$ ';
+    return 'jsterm1.0 $ ';
 };
 
 CONFIG.username = '';
